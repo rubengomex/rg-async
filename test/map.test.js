95eb633eb0d4a47046edb79a48c96aaa4209b68d
rubengomex <gomesruben21@gmail.com>
2017-08-14 09:34:13 +0100

    test(map): Add one more test to the map function.
    Add one more test to se if the map function is working an mapping any value based on the content of
    the mapper function.

--- test/map.test.js
+++ test/map.test.js
@@ -10,6 +10,11 @@ describe('map tests', () => {
     });
 
     it('it should return a mapped array with each item multiplied by 2', () => {
-        expect(rgAsync.map([1, 2, 3], async value => await Promise.resolve(value * 2))).to.eventually.be.deep.equal([2, 4, 6]);
+        expect(rgAsync.map([1, 2, 3], value => Promise.resolve(value * 2))).to.eventually.be.deep.equal([2, 4, 6]);
+    });
+
+    it('it should a return mapped array with each item as an object with name', () => {
+        expect(rgAsync.map(['rg', 'async', 'promise'], value => Promise.resolve({name: value})))
+        .to.eventually.be.deep.equal([{name: 'rg'}, {name: 'async'}, {name: 'promise'}]);
     });
 });
